422d0587cccdb769597a59f2c41c1a3f3f8faaaf
fran <fran-valero@hotmail.com>
2024-04-03 19:58:16 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const listInput = document.querySelector(".list-input");
+const addtBtn = document.querySelector(".list-btn");
+const listContainer = document.querySelector(".list-container");
+const emptyMsg = document.querySelector(".empty");
